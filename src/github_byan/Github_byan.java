cf029dc0573c93a98aa4ca23dfbb32edff6d7c1a
Melki Abyan <melki20ti@mahasiswa.pcr.ac.id>
2021-07-02 22:18:38 +0700

    kembali ke sebelumnya

--- src/github_byan/Github_byan.java
+++ src/github_byan/Github_byan.java
@@ -16,7 +16,6 @@ public class Github_byan {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hai Melki");
     }
     
 }
